21237a056fcf2583e9d8bee7a1a8bfe9a019fe19
Aditya Raj <araj67995@gmail.com>
2026-06-30 21:00:00 +0530

    to be conti.. in user

--- routes/user.js
+++ routes/user.js
@@ -2,16 +2,39 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const Booking = require("../models/booking");
-const {requireUser} = require("../utils/auth.js");
+const { requireUser } = require("../utils/auth");
+const { formatDateToDDMMYYYY } = require("../utils/date");
 
 router.use(requireUser);
 
-router.get("/", async(req, res) => {
-    const user = await User.findById(req.session.userId);
-    
-    res.render("user", {
-        user
+router.get("/", async (req, res) => {
+  const user = await User.findById(req.session.userId);
+
+  res.render("user", {
+    user,
+  });
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, email, phone, EventName, type, location, date, details } =
+      req.body;
+
+    const booking = new Booking({
+      name,
+      phone,
+      email,
+      EventName,
+      EventType: type,
+      location,
+      date: formatDateToDDMMYYYY(date),
+      details,
     });
+
+    await booking.save();
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
