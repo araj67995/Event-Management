2f8da776f5a89c8f6cd2d7407ab1703be73ccd3a
Aditya Raj <araj67995@gmail.com>
2026-07-01 20:39:46 +0530

    to be conti..

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router(); // ← invoke the Router function
 const { requireAdmin } = require("../utils/auth.js");
+const User = require("../models/user");
+const Booking = require("../models/booking");
+const { formatDateToDDMMYYYY } = require("../utils/date");
 
 router.use(requireAdmin);
 
